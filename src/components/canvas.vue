7b426f3ac8316848306536108468ad129784dae7
AsgerJon <asgerjon2@gmail.com>
2024-09-04 21:43:21 +0200

    Initial commit

---
+++ src/components/canvas.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: 'Canvas',
+  setup() {
+    const canvas = ref(null);
+
+    onMounted(() => {
+      const ctx = canvas.value.getContext('2d');
+      ctx.fillStyle = 'red';
+      ctx.fillRect(0, 0, 200, 200);
+    });
+
+    return {
+      canvas,
+    };
+  },
+};
+
+</script>
+
+<template>
+  <canvas ref="canvas" width="200" height="200"></canvas>
+</template>
+
+<style scoped>
+
+</style>
